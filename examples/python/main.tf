665330b843885fdfb456b37a1116bd7b6cd818c0
Radia Wong <radia.wong@datadoghq.com>
2025-08-05 13:50:30 -0400

    Revert "created check for var.template.volumes"
    This reverts commit 32222538e4ff1d87b4355268b6a470768c9cad80.
    
    committed wrong main.tf

--- examples/python/main.tf
+++ examples/python/main.tf
@@ -18,10 +18,6 @@ module "datadog-cloud-run-v2-wrapper" {
   dd_enable_logging = true
   dd_log_level = "debug"
   dd_logging_path = "/shared-volume/logs/*.log"
-  dd_shared_volume = {
-    name = "dd-shared-volume"
-    mount_path = "/shared-volume"
-  }
 
 
   dd_sidecar = {
@@ -49,13 +45,6 @@ module "datadog-cloud-run-v2-wrapper" {
           size_limit = "100Mi"
         }
       },
-      {
-        name = "dd-shared-volume"
-        empty_dir = {
-          medium = "MEMORY"
-          size_limit = "100Mi"
-        }
-      }
 
     ]
 
