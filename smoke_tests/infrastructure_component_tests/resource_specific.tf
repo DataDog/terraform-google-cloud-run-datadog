e150986fde24c52ad85561eabdd7a2755db72150
Ava Silver <ava.silver@datadoghq.com>
2025-08-28 17:15:51 -0400

    [SVLS-7388] add headers to all source files

--- smoke_tests/infrastructure_component_tests/resource_specific.tf
+++ smoke_tests/infrastructure_component_tests/resource_specific.tf
@@ -1,3 +1,6 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+# This product includes software developed at Datadog (https://www.datadoghq.com/) Copyright 2025 Datadog, Inc.
+
 ### Testing examples of different containers and volumes, infrastructure component setups (user-module value prioritization tested in logging_flag.tf)
 # - setting containers
 # - setting volumes
