9b937b5a16669989688fe98c17b940702b25fdc7
Tal Usvyatsky <tal.usvyatsky@datadoghq.com>
2026-04-27 09:44:48 -0400

    Add us2.ddog-gov.com site

--- variables.tf
+++ variables.tf
@@ -22,9 +22,10 @@ variable "datadog_site" {
         "ddog-gov.com",
         "ap1.datadoghq.com",
         "ap2.datadoghq.com",
+        "us2.ddog-gov.com",
       ],
     var.datadog_site)
-    error_message = "Invalid Datadog site. Valid options are: 'datadoghq.com', 'datadoghq.eu', 'us5.datadoghq.com', 'us3.datadoghq.com', 'ddog-gov.com', 'ap1.datadoghq.com', or 'ap2.datadoghq.com'."
+    error_message = "Invalid Datadog site. Valid options are: 'datadoghq.com', 'datadoghq.eu', 'us5.datadoghq.com', 'us3.datadoghq.com', 'ddog-gov.com', 'ap1.datadoghq.com', 'ap2.datadoghq.com', or 'us2.ddog-gov.com'."
   }
 }
 
